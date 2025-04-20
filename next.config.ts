a301c89be8b41ec1984192ac11f6831ff07a4bf3
Emre Taş <75886362+emretasss@users.noreply.github.com>
2025-04-20 17:43:23 +0300

    s

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   // Diğer Next.js ayarları (örneğin, statik site için output)
-  output: 'export', // Statik site için (eğer kullanıyorsanız)
+  output: 'standalone',
+
 };
 
 export default nextConfig;
